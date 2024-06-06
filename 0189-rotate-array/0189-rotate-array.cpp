2e7032af19096fd563f252ce3a5c026e5aef73b8
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-06-06 11:24:54 +0530

    Time: 3 ms (99.99%), Space: 28.1 MB (13.32%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k){
-        int n = nums.size();
-        int rotations = k % n, regular = 0;
-        if (rotations == 0)
-            return;
-        vector<int> ans(n);
-        for (int i = 0; i < n; ++i){
-            if (rotations > 0)
-                ans[i] = nums[n - rotations--];
-            else
-                ans[i] = nums[regular++];
+ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        vector<int>temp(nums.size());
+        for(int i=0;i<nums.size();i++){
+            temp[(i+k)%nums.size()]=nums[i];
         }
-        nums = ans;
+        nums=temp;
     }
 };
