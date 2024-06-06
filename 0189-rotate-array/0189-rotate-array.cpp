e351898aee0df29f76ae746fed9c8630dddfe388
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-06-06 11:25:15 +0530

    Time: 13 ms (95.33%), Space: 28 MB (24.55%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k){
-        // ios::sync_with_stdio(false);
-        // cin.tie(nullptr);
-        // cout.tie(nullptr);
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         vector<int>temp(nums.size());
         for(int i=0;i<nums.size();i++){
             temp[(i+k)%nums.size()]=nums[i];
