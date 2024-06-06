39ae79cd200c993c07ce46b44bc80bf46417ce2b
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-06-06 12:35:05 +0530

    Time: 10 ms (85.14%), Space: 21 MB (13.58%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size(); i++){
+            if(i!=nums[i]){
+                return i;
+                break;
+            }
+        }
+        return nums.size();
+    }
+};
