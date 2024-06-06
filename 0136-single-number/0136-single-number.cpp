b2c9cb773506ced8cfcdfd29ba723b823553b58c
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-06-06 19:26:09 +0530

    Time: 7 ms (96.19%), Space: 19.1 MB (98.53%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-    int n = nums.size();
-
-    // Declare the hashmap.
-    // And hash the given array:
-    map<int, int> mpp;
-    for (int i = 0; i < n; i++) {
-        mpp[nums[i]]++;
-    }
-
-    //Find the single element and return the answer:
-    for (auto it : mpp) {
-        if (it.second == 1)
-            return it.first;
-    }
-
-    //This line will never execute
-    //if the array contains a single element.
-    return -1;
+    int res=0;
+        for(int i=0;i<nums.size();i++){
+            res = res^nums[i];
+        }
+        return res;
     }
 };
