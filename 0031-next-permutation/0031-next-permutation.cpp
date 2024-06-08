8cd49e0fe2f2f432d8389283442a11240b192246
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-06-08 22:24:32 +0530

    Time: 0 ms (100%), Space: 14.7 MB (14.72%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) 
     {
+       std::ios_base::sync_with_stdio(false);
+       std::cout.tie(0);
        next_permutation(nums.begin(),nums.end());
     }
 };
