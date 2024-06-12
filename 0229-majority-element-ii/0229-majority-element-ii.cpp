2b6acf79c0602dd83ddd88bc24b4fd00f4e4b99e
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-06-12 13:01:17 +0530

    Time: 5 ms (86.52%), Space: 18.4 MB (44.9%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -6,19 +6,25 @@ public:
         int el1=INT_MIN,el2=INT_MIN;
 
         for(int i=0; i<nums.size(); i++){
-        if (cnt1 == 0 && el2 != nums[i]) {
-            cnt1 = 1;
-            el1 = nums[i];
-        }
-        else if (cnt2 == 0 && el1 != nums[i]) {
-            cnt2 = 1;
-            el2 = nums[i];
-        }
-        else if (nums[i] == el1) cnt1++;
-        else if (nums[i] == el2) cnt2++;
-        else {
-            cnt1--, cnt2--;
-        }
+            if(cnt1==0 && el2!=nums[i])
+            {
+                cnt1=1;
+                el1=nums[i];
+            }
+            else if(cnt2==0 && el1!=nums[i]){
+                cnt2=1;
+                el2=nums[i];                
+            }
+            else if(el1==nums[i]){
+                cnt1++;
+            }
+            else if(el2==nums[i]){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
         }
 
         vector<int> ls;
