d6c0da394710b1f7b6b0ad7cac2886776f78aeb8
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-06-12 12:57:31 +0530

    Time: 14 ms (16.76%), Space: 18.2 MB (91.59%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,14 +2,10 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         
-    int n = nums.size(); //size of the array
+        int cnt1=0 , cnt2=0;
+        int el1=INT_MIN,el2=INT_MIN;
 
-    int cnt1 = 0, cnt2 = 0; // counts
-    int el1 = INT_MIN; // element 1
-    int el2 = INT_MIN; // element 2
-
-    // applying the Extended Boyer Moore's Voting Algorithm:
-    for (int i = 0; i < n; i++) {
+        for(int i=0; i<nums.size(); i++){
         if (cnt1 == 0 && el2 != nums[i]) {
             cnt1 = 1;
             el1 = nums[i];
@@ -23,26 +19,18 @@ public:
         else {
             cnt1--, cnt2--;
         }
-    }
+        }
 
-    vector<int> ls; // list of answers
+        vector<int> ls;
+        cnt1 = 0, cnt2 = 0;
+        for (int num : nums) {
+            if (num == el1) cnt1++;
+            if (num == el2) cnt2++;
+        }
+        int mini = int(nums.size() / 3) + 1;
+        if (cnt1 >= mini) ls.push_back(el1);
+        if (cnt2 >= mini) ls.push_back(el2);
 
-    // Manually check if the stored elements in
-    // el1 and el2 are the majority elements:
-    cnt1 = 0, cnt2 = 0;
-    for (int i = 0; i < n; i++) {
-        if (nums[i] == el1) cnt1++;
-        if (nums[i] == el2) cnt2++;
-    }
-
-    int mini = int(n / 3) + 1;
-    if (cnt1 >= mini) ls.push_back(el1);
-    if (cnt2 >= mini) ls.push_back(el2);
-
-    // Uncomment the following line
-    // if it is told to sort the answer array:
-    // sort(ls.begin(), ls.end()); //TC --> O(2*log2) ~ O(1);
-
-    return ls;
+        return ls;
     }
 };
