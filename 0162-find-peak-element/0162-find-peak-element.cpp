3f679eaf670e83d9f67f0073fb52bff65c93a196
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-06-20 16:21:50 +0530

    Time: 0 ms (100%), Space: 11.2 MB (50.99%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -5,25 +5,17 @@ public:
 
     // Edge cases:
     if (n == 1) return 0;
-    if (nums[0] > nums[1]) return 0;
-    if (nums[n - 1] > nums[n - 2]) return n - 1;
 
-    int low = 1, high = n - 1;
-    while (low <= high) {
-        int mid = (low + high) / 2;
-
-        //If arr[mid] is the peak:
-        if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1])
-            return mid;
+    int low = 0, high = n - 1;
+    while (low < high) {
+        int mid = low + (high-low) / 2;
 
         // If we are in the left:
-        if (nums[mid] > nums[mid - 1]) low = mid + 1;
+        if (nums[mid] < nums[mid + 1]) low = mid + 1;
 
-        // If we are in the right:
-        // Or, arr[mid] is a common point:
-        else high = mid - 1;
+        else high = mid;
     }
     // Dummy return statement
-    return -1;        
+    return low;        
     }
 };
