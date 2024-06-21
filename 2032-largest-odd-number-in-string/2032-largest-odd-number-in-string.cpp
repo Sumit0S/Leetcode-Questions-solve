f8600e484850c2dd62127215b42ccdcad6a2dc7a
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-06-21 13:55:15 +0530

    Time: 29 ms (52.27%), Space: 16.4 MB (23.47%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
    string largestOddNumber(string num)
 {
-    int ans = 0;
+    string ans="";
+
     int n = num.size()-1;
-    cout<<n<<endl;
+
     for (int i = n; i >= 0; i--)
     {
         if ((num[i] - '0') & 1)
@@ -12,6 +13,6 @@ public:
             return num.substr(0, i+1);
         }
     }
-    return "";
+    return ans;
  }
 };
