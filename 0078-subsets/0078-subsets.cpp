dfaf80d68c606f51dacffbacb0c26ecc9b71140c
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-07-03 12:52:29 +0530

    Time: 2 ms (51.86%), Space: 13.9 MB (20.48%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void subsets(vector<int> &nums,int i,vector<int> output,vector<vector<int> > &ans){
+        if(i==nums.size()){
+            ans.push_back(output);
+            return;
+        }
+
+        // exclude
+        subsets(nums,i+1,output,ans);
+        // include
+        int a=nums[i];
+        output.push_back(a);
+        subsets(nums,i+1,output,ans);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int> > ans;
+        vector<int> output;
+        subsets(nums,0,output,ans);
+        return ans;
+    }
+};
