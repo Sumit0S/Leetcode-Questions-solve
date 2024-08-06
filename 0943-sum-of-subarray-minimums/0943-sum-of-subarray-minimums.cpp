31d405e101bbaa91573798219e4e05f4d3e60f20
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-08-07 01:13:58 +0530

    Time: 109 ms (9.81%), Space: 45.9 MB (33.73%) - LeetHub

---
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+
+vector<int> presmaler(vector<int>& arr)
+   
+{
+
+        stack<int> st;
+        vector<int> v(arr.size());
+        for(int i=0; i<arr.size(); i++)
+        {
+            while(!st.empty() and arr[i]<=arr[st.top()])
+            {
+                st.pop();
+            }
+            v[i]=st.empty()?-1:st.top();
+            st.push(i); 
+        }
+        return v;
+
+}
+
+vector<int> nextsmaler(vector<int>& arr)
+   
+{
+
+        stack<int> st;
+        vector<int> v(arr.size());
+        for(int i=arr.size()-1; i>=0; i--)
+        {
+            while(!st.empty() and arr[i]<arr[st.top()])
+            {
+                st.pop();
+            }
+            v[i]=st.empty()?arr.size():st.top();
+            st.push(i); 
+        }
+        return v;
+
+}
+
+int sumSubarrayMins(vector<int>& arr) 
+    {
+        long long mod=1000000007;
+        int n=arr.size();
+        vector<int> a = presmaler(arr);
+        vector<int> b = nextsmaler(arr);
+        long long total=0;
+
+        for(int i=0; i<n; i++){
+            cout<<b[i];
+        }
+        for(int i=0; i<n; i++){
+            int left=i-a[i];
+            int right=b[i]-i;
+            total = (total + arr[i] * 1LL * left * right % mod) % mod;
+        }
+        return static_cast<int>(total);
+    }
+};
