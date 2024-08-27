a79311623fbcf84bbd3e05e4990a48d48b585a28
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-08-27 23:57:46 +0530

    Time: 4 ms (42.35%), Space: 9.4 MB (61.78%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -2,43 +2,47 @@ class Solution {
 public:
 
     int calculateval(vector<int> &a, int maxSum) {
-        int n = a.size(); // Size of array
+        int n = a.size(); //size of array.
         int partitions = 1;
         long long subarraySum = 0;
 
         for (int i = 0; i < n; i++) {
             if (subarraySum + a[i] <= maxSum) {
-                // Insert element to current subarray
+                //insert element to current subarray
                 subarraySum += a[i];
-            } else {
-                // Insert element to next subarray
+            }
+            else {
+                //insert element to next subarray
                 partitions++;
+               
                 subarraySum = a[i];
             }
         }
         return partitions;
     }
-
     int splitArray(vector<int>& nums, int k) {
-        int mini = *max_element(nums.begin(), nums.end()); // Maximum element in the array
-        int max2 = accumulate(nums.begin(), nums.end(), 0); // Sum of all elements
+        int mini=0;
+        int max2=0;
+        
+        for(int i=0; i<nums.size(); i++){
+            mini=max(mini,nums[i]);
+            max2+=nums[i];
+        }
 
-        int low = mini;
-        int high = max2;
-        int result = high; // Initialize with a possible upper bound
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int val = calculateval(nums, mid);
-            
-            if (val <= k) {
-                result = mid; // Store the current mid as it's a valid maximum sum
-                high = mid - 1; // Try to find a smaller valid maximum sum
-            } else {
-                low = mid + 1; // Increase the allowed maximum sum
+        int low=mini;
+        int high=max2;
+        int result = high;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int val=  calculateval(nums,mid);
+            if(val<=k){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
 
-        return result;
+        return low;
     }
 };
