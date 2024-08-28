ba0a4120023a23eb0d1abbdcf99158643d614269
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-08-28 17:04:38 +0530

    Time: 211 ms (23.95%), Space: 131.4 MB (19.73%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) 
+    {
+        vector<pair<int, int>> projects;
+        for(int i = 0; i < profits.size(); i++) {
+            projects.push_back({capital[i], profits[i]});
+        }
+
+        // Sort projects by the capital required to start
+        sort(projects.begin(), projects.end());
+
+        priority_queue<int> maxProfitQueue;
+        int j = 0;
+
+        for(int i = 0; i < k; i++) {
+            // Push all projects that can be started with the current capital to the max heap
+            while(j < projects.size() && projects[j].first <= w) {
+                maxProfitQueue.push(projects[j].second);
+                j++;
+            }
+
+            // If no project can be started, break early
+            if(maxProfitQueue.empty()) {
+                break;
+            }
+
+            // Add the most profitable project
+            w += maxProfitQueue.top();
+            maxProfitQueue.pop();
+        }
+
+        return w;
+    }
+};
