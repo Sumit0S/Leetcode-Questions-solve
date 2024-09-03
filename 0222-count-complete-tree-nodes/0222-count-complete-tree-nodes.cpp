f418a059e0ec3f6dd3dee46d09aa85f1d5556467
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-09-03 19:33:15 +0530

    Time: 25 ms (42.12%), Space: 29.4 MB (35%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -9,26 +9,60 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+                                
+
 class Solution {
 public:
-    void count(TreeNode* root,int &cnt){
-        if(root==NULL){
-            
-            return;
-        }
-        cnt+=1;
-        count(root->left,cnt);
-        count(root->right,cnt);
-    }
+    // Function to count nodes
+    // in a binary tree
     int countNodes(TreeNode* root) {
-        int cnt=0;
-        if(root==NULL){
+        // Base case: If the root
+        // is NULL, there are no nodes
+        if (root == NULL) {
             return 0;
         }
-        else{
-            count(root,cnt);
-            return cnt;
+        
+        // Find the left height and
+        // right height of the tree
+        int lh = findHeightLeft(root);
+        int rh = findHeightRight(root);
+        
+        // Check if the last level
+        // is completely filled
+        if (lh == rh) {
+            // If so, use the formula for
+            // total nodes in a perfect
+            // binary tree ie. 2^h - 1
+            return (1 << lh) - 1; 
         }
-
+        
+        // If the last level is not completely
+        // filled, recursively count nodes in
+        // left and right subtrees
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+    
+    // Function to find the left height of a tree
+    int findHeightLeft(TreeNode* node) {
+        int height = 0;
+        // Traverse left child until
+        // reaching the leftmost leaf
+        while (node) {
+            height++;
+            node = node->left;
+        }
+        return height;
+    }
+    
+    // Function to find the right height of a tree
+    int findHeightRight(TreeNode* node) {
+        int height = 0;
+        // Traverse right child until
+        // reaching the rightmost leaf
+        while (node) {
+            height++;
+            node = node->right;
+        }
+        return height;
     }
 };
