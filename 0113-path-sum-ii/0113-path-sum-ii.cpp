0113e0789a0764eba0450c77602a1672ae167166
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-09-04 18:16:15 +0530

    Time: 24 ms (5.06%), Space: 38.6 MB (6.79%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -11,31 +11,25 @@
  */
 class Solution {
 public:
-
-    bool pathsum(TreeNode* root, int targetSum,vector<int> &v,int &sum,vector<vector<int>> &ans){
-        if(root==NULL){
-            return true;
+    void helper(TreeNode* root,vector<int>v,vector<vector<int>>&ans,int sum){
+        if(root==NULL) return;
+        // this condition specify that we are in leaf node.because right & left of leaf node is NULL 
+        if(root->left==NULL && root->right==NULL){
+            if(root->val==sum){
+                v.push_back(root->val);
+                ans.push_back(v);
+            }
+            
         }
-
         v.push_back(root->val);
-        sum+=root->val;
-
-        bool left=pathsum(root->left,targetSum,v,sum,ans);
-        bool right=pathsum(root->right,targetSum,v,sum,ans);
-        if(left and right and targetSum==sum){
-            ans.push_back(v);
-        }
-
-        v.pop_back();
-        sum-=root->val;
-        return false;
-
+        // Recursively call the helper function for left and right subtrees
+        helper(root->left,v,ans,sum-root->val);
+        helper(root->right,v,ans,sum-root->val);
     }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<vector<int>> ans;
-        vector<int> v;
-        int sum=0;
-        pathsum(root,targetSum,v,sum,ans);
+        vector<vector<int>>ans;
+        vector<int>v;
+        helper(root,v,ans,targetSum);
         return ans;
     }
 };
