aced24bb2b89ca3984a2b331e8a0517188495c7f
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-09-09 01:10:23 +0530

    Time: 0 ms (100%), Space: 16.1 MB (89.11%) - LeetHub

--- 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,38 +11,36 @@
  */
 class Solution {
 public:
-
-    void make(TreeNode* &dummy,int val){
-        TreeNode* root=dummy;
-        while(root!=NULL){
-            if(val<root->val){
-                if(root->left!=NULL){
-                    root=root->left;
-                }
-                else{
-                    root->left=new TreeNode(val);
-                    break;
-                }
-            }
-            else{
-                if(root->right!=NULL){
-                    root=root->right;
-                }
-                else{
-                    root->right=new TreeNode(val);
-                    break;
-                }
-                
-            }
+    // Function to insert a new value into the binary search tree
+    void insert(TreeNode* root, int val) {
+        // If the value is less than the current node's value, go to the left subtree
+        if(val < root->val) {
+            // If there is no left child, create a new node and attach it as the left child
+            if(root->left == NULL) root->left = new TreeNode(val);
+            // If a left child exists, recursively insert the value into the left subtree
+            else insert(root->left, val);
         }
-
+        // If the value is greater than or equal to the current node's value, go to the right subtree
+        else {
+            // If there is no right child, create a new node and attach it as the right child
+            if(root->right == NULL) root->right = new TreeNode(val);
+            // If a right child exists, recursively insert the value into the right subtree
+            else insert(root->right, val);
+        }
     }
-    TreeNode* bstFromPreorder(vector<int>& preorder) 
-    {
-        TreeNode* root=new TreeNode(preorder[0]);
-        for(int i=1; i<preorder.size(); i++){
-            make(root,preorder[i]);
+
+    // Function to build a BST from a given preorder traversal array
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        // Create the root node with the first element of the preorder array
+        TreeNode* root = new TreeNode(preorder[0]);
+        
+        // Iterate through the rest of the elements in the preorder array
+        for(int i = 1; i < preorder.size(); i++) {
+            // Insert each element into the BST
+            insert(root, preorder[i]);
         }
+        
+        // Return the root of the constructed BST
         return root;
     }
 };
