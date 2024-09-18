f077063fee78a9f298bac4899a5b74b4622a5cd3
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-09-18 20:59:15 +0530

    Time: 5 ms (22.91%), Space: 9 MB (68.71%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,27 +1,30 @@
 class Solution {
-    void removeLeadingSpace(string &s, int &i){
+public:
+    void ignorespace(string &s,int &i){
         while(i < s.size() && s[i] == ' ') i++;
     }
-public:
     int myAtoi(string s) {
-        if(s.size() == 0) return 0;
-        int i = 0;
-
-        removeLeadingSpace(s, i);
-
+        int i=0;
+        ignorespace(s,i);
         if(i == s.size()) return 0;
+        
+        long long maxi=INT_MAX,mini=INT_MIN,ans=0,sign=1;
 
-        long long mini = INT_MIN, maxi = INT_MAX, ans = 0, sign = 1;
-
-        if(s[i] == '-'){
-            sign = -1;
+        if(s[i]=='-'){
             i++;
-        }else if(s[i] == '+') i++;
-        for(; i<s.size(); i++){
-            if(s[i] < '0' || s[i] > '9') return ans;
-            ans = ans * 10 + (sign*(s[i] - '0'));
+            sign*=-1;
+        }
+        else if(s[i]=='+'){
+            i++;
+        }
+        while(i<s.size()){
+            if(s[i]<'0' || s[i]>'9'){
+                return ans;
+            }
+            ans=ans*10+(sign*(s[i]-'0'));
             if(ans <= mini) return mini;
             if(ans >= maxi) return maxi;
+            i++;
         }
         return ans;
     }
