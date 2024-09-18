7a84577e4f74b5c941472dd096b8cdf7618bf0aa
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-09-18 20:38:29 +0530

    Time: 0 ms (100%), Space: 9.1 MB (27.69%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -6,16 +6,20 @@ public:
     int myAtoi(string s) {
         if(s.size() == 0) return 0;
         int i = 0;
+
         removeLeadingSpace(s, i);
+
         if(i == s.size()) return 0;
+
         long long mini = INT_MIN, maxi = INT_MAX, ans = 0, sign = 1;
+
         if(s[i] == '-'){
-            sign *= -1;
+            sign = -1;
             i++;
         }else if(s[i] == '+') i++;
         for(; i<s.size(); i++){
             if(s[i] < '0' || s[i] > '9') return ans;
-            ans = ans * 10 + (sign * (s[i] - '0'));
+            ans = ans * 10 + (sign*(s[i] - '0'));
             if(ans <= mini) return mini;
             if(ans >= maxi) return maxi;
         }
