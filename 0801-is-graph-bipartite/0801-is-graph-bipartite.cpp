040c8cbbe88a4f9d244f8f6b521277a07e4c5b16
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-09-27 10:55:44 +0530

    Time: 15 ms (80.46%), Space: 16.9 MB (21.12%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
 
-    bool dfs(int node,int col,int color[],vector<vector<int>>& graph)
+    bool bfs(int u,vector<int>&vis,vector<int>&color,vector<vector<int>>& A)
     {
-        color[node]=col;
-        for( auto it : graph[node]){
-            if(color[it]==-1){
-                if(dfs(it,!col,color,graph)==false){
-                    return false;
-                }
-            }
-            else if(color[it]==col){
-                return false;
-            }
-        }
-        return true;
-    }
-    bool isBipartite(vector<vector<int>>& graph) 
-    {
-        int v=graph.size();
-        int m=graph[0].size();
+        queue<int>q;
 
-        int color[v];
-        for(int i=0; i<v; i++){
-            color[i]=-1;
-        }
+        q.push(u);
+        
+        vis[u]=1,color[u]=0;
 
-        for(int i=0; i<v; i++)
+        while(!q.empty())
+        
         {
-            if(color[i]==-1)
-            {
-                if(dfs(i,0,color,graph)==false)
-                {
-                    return false;
-                }
-            }
+        
+            int u=q.front();q.pop();
+
+            for(auto& v: A[u])
+                if(!vis[v])
+                    vis[v]=1,
+                    color[v]=!color[u],
+                    q.push(v);
+                else if(color[v]==color[u])
+                    return 0;
         }
-        return true;
+        return 1;
+    }
+
+public:
+    bool isBipartite(vector<vector<int>>& A) 
+    {
+
+        int n=A.size();
+
+        vector<int> vis(n,0) , color(n,-1);
+
+        queue<int>q;
+        for(int i=0;i<n;i++)
+            if(!vis[i])
+                if(!bfs(i,vis,color,A)) return 0;
+        return 1;
     }
 };
