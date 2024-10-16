9974890a6aaecbcac4659219174bdb7a415e6343
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-10-16 23:03:16 +0530

    Time: 64 ms (25.89%), Space: 36.5 MB (50.76%) - LeetHub

---
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads)
+    {
+        vector<pair<int,int>> adj[n];
+
+        for(auto it:roads){
+            int i=it[0];
+            int j=it[1];
+            int w=it[2];
+            adj[i].push_back({j,w});
+            adj[j].push_back({i,w});
+        }
+
+        set<pair<long long, int>> st;  // Using long long for distances
+        st.insert({0,0});
+        vector<int> ways(n,0);
+        vector<long long> dist(n,1e18);  // Use long long to handle large distances
+        ways[0]=1;
+        dist[0]=0;
+
+        int mod=(int)(1e9+7);
+        while(!st.empty()){
+            auto it = *(st.begin());
+            long long dst=it.first;
+            int node=it.second;
+            st.erase(it);  // Erase the specific iterator
+            for(auto it:adj[node]){
+                int nn=it.first;
+                int nd=it.second;
+                if(dst+nd<dist[nn]){
+                    dist[nn]=dst+nd;
+                    ways[nn]=ways[node];
+                    st.insert({dst+nd,nn});
+                }
+                else if(dst+nd==dist[nn]){
+                    ways[nn]=(ways[nn]+ways[node])%mod;
+                }
+            }
+        }   
+        return ways[n-1]%mod; 
+    }
+};
