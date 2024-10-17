bc12659371d4a3c96e249da5480ecaf17dc47a01
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-10-17 23:14:08 +0530

    Time: 0 ms (100%), Space: 7.7 MB (31.77%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
+    int maximumSwap(int num) {
+        int maxIndex = -1, swap1 = -1, swap2 = -1;
+        std::string nums = std::to_string(num);
+        int n = nums.size();
 
-    int maximumSwap(int num)
-    {
-        string s = "";
-        while(num > 0) {
-            s += (num % 10+'0');
-            num = num / 10;
-        }
-        reverse(s.begin(),s.end());
-
-        for(int i=0; i<s.length(); i++){
-            int maxi=i;
-            for(int j=i+1; j<s.length(); j++){
-                if(s[i]<s[j] and s[maxi]<=s[j]){
-                    maxi=j;
-                }
-            }
-            if(i!=maxi){
-                swap(s[i],s[maxi]);
-                break;
+        for (int i = n - 1; i >= 0; i--) {
+            if (maxIndex == -1 || nums[i] > nums[maxIndex]) {
+                maxIndex = i;
+            } 
+            else if (nums[i] < nums[maxIndex]) {
+                swap1 = i;
+                swap2 = maxIndex;
             }
         }
-        int num2 = stoi(s);
-        return num2;
+
+        if (swap1 != -1 && swap2 != -1) {
+            std::swap(nums[swap1], nums[swap2]);
+        }
+
+        return std::stoi(nums);
     }
 };
