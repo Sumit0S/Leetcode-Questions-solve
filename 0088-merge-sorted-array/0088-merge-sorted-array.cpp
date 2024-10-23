dc886671160ec6049b29e755991c8feba4d7f212
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-10-24 02:17:07 +0530

    Time: 0 ms (100%), Space: 11.4 MB (21.56%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -7,10 +7,10 @@ public:
 
         // Start merging from the back to avoid overwriting elements in nums1
         while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
-            } else {
+            if (nums1[i] < nums2[j]) {
                 nums1[k--] = nums2[j--];
+            } else {
+                nums1[k--] = nums1[i--];
             }
         }
 
