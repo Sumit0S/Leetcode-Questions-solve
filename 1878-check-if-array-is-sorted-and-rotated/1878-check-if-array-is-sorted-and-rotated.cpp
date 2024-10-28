e7b102d63c506d1cffde379b6e5d78f18cfbbc66
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-10-28 21:17:21 +0530

    Time: 0 ms (100%), Space: 10.3 MB (96.75%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -4,13 +4,16 @@ public:
         int cnt = 0; 
         int n = nums.size();
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > nums[(i + 1) % n]) {
+        for (int i = 0; i < n-1; i++) {
+            if (nums[i] > nums[i + 1]) {
                 cnt++;
-                if (cnt > 1) return false;
+                
             }
         }
-        
+        if(nums[n-1]>nums[0]){
+            cnt++;
+        }
+        if (cnt > 1) return false;
         return true; // The array is a rotated sorted array
     }
 };
