c514efc2a0eb6c116166f4c58543f16bd6ec7b34
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-10-28 21:30:15 +0530

    Time: 0 ms (100%), Space: 21.8 MB (10.1%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,24 +3,24 @@ public:
     int removeDuplicates(vector<int>& nums) 
     {
         sort(nums.begin(),nums.end());
-        if(nums.size()<2){
-            return nums.size();
+        int j=1;
+
+        if(nums.size()<=1){
+            return 1;
         }
-        int j=0;
+
         for(int i=1; i<nums.size(); i++){
-            if(nums[j]==nums[i]){
-                continue;
-            }
-            else{
-                j+=1;
+            if(nums[i-1]!=nums[i]){
                 nums[j]=nums[i];
-                // j+=1;
+                j++;
             }
         }
-        // if(nums[nums.size()-1]!=nums[nums.size()-2]){
-        //         nums[j]=nums[nums.size()-1];
-        //         j+=1;
+
+        // if(nums[j]!=nums[nums.size()-1]){
+        //     nums[j]=nums[nums.size()-1];
+        //     j++;
         // }
-        return j+1;    
+        cout<<j;
+        return j;
     }
 };
