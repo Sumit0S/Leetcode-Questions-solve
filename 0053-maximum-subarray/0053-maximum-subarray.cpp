e6cdb6a3c001d52a76d79e7444c3c9d97222f72b
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-10-29 20:45:22 +0530

    Time: 0 ms (100%), Space: 70.6 MB (21.17%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,9 +4,10 @@ public:
     {
         long int ans=nums[0];
         long int sum=0;
-        if(nums.size()==1){
-            return nums[0];
-        }
+        // if(nums.size()==1){
+        //     return nums[0];
+        // }
+        
         for(int i=0; i<nums.size(); i++){
             sum+=nums[i];
             if(sum<0){
