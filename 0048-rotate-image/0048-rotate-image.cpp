1e6b868ba40ae0502e0d879fd7922689249ef430
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-10-31 16:05:01 +0530

    Time: 0 ms (100%), Space: 8.8 MB (96.4%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-       std::ios_base::sync_with_stdio(false);
-       std::cout.tie(0);
-        int row=matrix.size();
-        int col=matrix[0].size();
-
-        for(int i=0; i<row; i++){
-            for(int j=i+1; j<col; j++){
-
-                swap(matrix[i][j],matrix[j][i]);
-            }
+    void rotate(vector<vector<int>>& matrix) 
+    {
+        int n = matrix.size();
+        //transposing the matrix
+        for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            swap(matrix[i][j], matrix[j][i]);
         }
-
-        for(int i=0; i<row; i++ ){
-            reverse(matrix[i].begin(),matrix[i].end());
         }
-
+        //reversing each row of the matrix
+        for (int i = 0; i < n; i++) 
+        {
+        reverse(matrix[i].begin(), matrix[i].end());
+        }
     }
 };
