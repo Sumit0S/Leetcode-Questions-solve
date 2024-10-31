f0beca1e1f919de10c45150b6b1dbbdbb220cf3d
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-10-31 17:13:13 +0530

    Time: 0 ms (100%), Space: 8.1 MB (90.84%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-       std::ios_base::sync_with_stdio(false);
-       std::cout.tie(0);
- vector<vector<int>> v(numRows); // Initialize the outer vector with numRows rows
-        
-        for(int i = 0; i < numRows; i++) {
-            v[i].resize(i + 1); // Resize each row to have (i+1) elements
-            v[i][0] = v[i][i] = 1; // Set the first and last elements of each row to 1
-            
-            // Fill in the values for the rest of the row
-            for(int j = 1; j < i; j++) {
-                v[i][j] = v[i - 1][j - 1] + v[i - 1][j]; // Compute the value based on the previous row
+    vector<vector<int>> generate(int numRows)
+    {
+        vector<vector<int>> ans(numRows,vector<int> (numRows,1));
+        for(int i=0; i<numRows; i++){
+            ans[i].resize(i+1);
+            for(int j=1; j<i; j++){
+                ans[i][j]=ans[i-1][j]+ans[i-1][j-1];
             }
         }
-        
-        return v;
+
+
+        return ans;
     }
 };
