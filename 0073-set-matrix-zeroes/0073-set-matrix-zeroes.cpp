a3250eb570820447a6e7ad475fadc8caa8742762
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-10-31 13:24:15 +0530

    Time: 0 ms (100%), Space: 17 MB (56.11%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,49 +1,37 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int size=matrix.size();
-        int var=1;
-        if(size==0){
-            return;
-        }
-        for(int i=0; i<size; i++){
-            for(int j=0; j<matrix[0].size(); j++){
-                if(matrix[i][j]==0){
+    void setZeroes(vector<vector<int>>& matrix)
+    {
+    
+    int n=matrix.size();
+    int m=matrix[0].size();
 
-                    matrix[i][0]=0;
-                    
-                    if(j!=0){
-                    matrix[0][j]=0;
-                    }
-                    
-                    else{
-                        var=0;
-                    }
-                }
-            }
-        }
+    vector<int> row(n,0); // row array
+    vector<int> col(m,0);
+ 
 
-        for(int i=1; i<size; i++){
-            for(int j=1; j<matrix[0].size(); j++){
-                if(matrix[i][j]!=0){
-                if(matrix[0][j]==0 || matrix[i][0]==0){
-                    matrix[i][j]=0;
-                }
-                }
-            }
-        }
-        if(matrix[0][0]==0){
-            for(int i=0; i<matrix[0].size(); i++){
-                matrix[0][i]=0;
-            }
-        }
+    // Traverse the matrix:
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 0) {
+                // mark ith index of row wih 1:
+                row[i] = 1;
 
-        
-        if(var==0){
-            for(int i=0; i<matrix.size(); i++){
-                matrix[i][0]=0;
+                // mark jth index of col wih 1:
+                col[j] = 1;
             }
-        }      
+        }
+    }
+
+    // Finally, mark all (i, j) as 0
+    // if row[i] or col[j] is marked with 1.
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (row[i] || col[j]) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
 
     }
 };
