3f411fdbb168102fee4abe72d9d9db9fef9b9639
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-11-02 15:58:59 +0530

    Time: 0 ms (100%), Space: 8.1 MB (83.91%) - LeetHub

---
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+
+        int n=sentence.size();
+
+        for(int i=1; i<n; i++){
+            if(sentence[i]==' '){
+                if(sentence[i-1]!=sentence[i+1]){
+                    return false;
+                }
+            }
+        }
+
+                if(sentence[0]==sentence[sentence.size()-1]){
+                        return true;
+                    }
+                else{
+                        return false;
+                }
+
+        return true;
+    }
+};
