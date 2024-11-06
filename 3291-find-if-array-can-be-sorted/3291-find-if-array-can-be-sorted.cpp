2e8c05d240756afa15a5448854d446cc889402b8
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-11-06 17:11:39 +0530

    Time: 0 ms (100%), Space: 31.2 MB (86.44%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
-    int countSetBits(int n) {
-        int count = 0;
-        while (n) {
-            count += n & 1;
-            n >>= 1;
-        }
-        return count;
-    }
-
-    bool canSortArray(vector<int>& nums) {
-
-
-        for(int i=0; i<nums.size(); i++){
-
-            if(i+1<nums.size() and countSetBits(nums[i])==countSetBits(nums[i+1]) and nums[i]>nums[i+1]){
-                swap(nums[i],nums[i+1]);   
-                i=0; 
-            }
-
-            else if(i>0 and countSetBits(nums[i])==countSetBits(nums[i-1]) and nums[i]<nums[i-1]){
-                swap(nums[i],nums[i-1]); 
-                    i=0;   
-            }
-
-        }
-
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i-1]>nums[i]){
+    static bool canSortArray(const vector<int> &nums) {
+        const uint8_t n = nums.size();
+        uint16_t pmax = 0, cmin = 0, cmax = 0;
+        uint8_t pcnt = 0;
+        for (const uint16_t v : nums) {
+            if (const uint8_t ccnt = popcount(v); pcnt == ccnt) {
+                cmin = min(cmin, v);
+                cmax = max(cmax, v);
+            } else if (cmin < pmax) {
                 return false;
+            } else {
+                pmax = cmax;
+                cmin = cmax = v;
+                pcnt = ccnt;
             }
         }
-        return true;
+        return cmin >= pmax;
     }
 };
