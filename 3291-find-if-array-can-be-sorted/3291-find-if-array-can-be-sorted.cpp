dce522cfb6665c71bf754a04979ecbbdbb4eadce
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-11-06 21:51:26 +0530

    Time: 0 ms (100%), Space: 31.3 MB (63.03%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-    static bool canSortArray(const vector<int> &nums) {
-        const uint8_t n = nums.size();
-        uint16_t pmax = 0, cmin = 0, cmax = 0;
-        uint8_t pcnt = 0;
-        for (const uint16_t v : nums) {
-            if (const uint8_t ccnt = popcount(v); pcnt == ccnt) {
-                cmin = min(cmin, v);
-                cmax = max(cmax, v);
-            } else if (cmin < pmax) {
-                return false;
+    bool canSortArray(std::vector<int>& nums) {
+        int prevMax = INT_MIN;
+        int currMax = nums[0];
+        int currMin = nums[0];
+        int setBits = countBits(nums[0]);
+
+        for (size_t i = 1; i < nums.size(); i++) {
+            if (setBits == countBits(nums[i])) {
+                currMax = std::max(currMax, nums[i]);
+                currMin = std::min(currMin, nums[i]);
             } else {
-                pmax = cmax;
-                cmin = cmax = v;
-                pcnt = ccnt;
+                if (currMin < prevMax)
+                    return false;
+
+                prevMax = currMax;
+                setBits = countBits(nums[i]);
+                currMin = nums[i];
+                currMax = nums[i];
             }
         }
-        return cmin >= pmax;
+
+        return currMin > prevMax;
+    }
+
+private:
+    int countBits(int num) {
+        return std::bitset<32>(num).count();
     }
 };
