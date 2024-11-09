1f6b7e549993ee379be27371de8c69a7413c497b
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-11-09 22:04:27 +0530

    Time: 7 ms (37.76%), Space: 8.9 MB (55.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+    bool isAnagram(string s, string t) 
+    {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        cout<<s<<" "<<t;
+        for(int i=0; i<s.length(); i++)
+        {
+            if(s[i]!=t[i]){
+                return false;
+            }
+        }
+        return true;
     }
 };
