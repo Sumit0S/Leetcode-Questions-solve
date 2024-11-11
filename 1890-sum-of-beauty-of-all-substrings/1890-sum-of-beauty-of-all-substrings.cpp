e2fa0092ccabc9eb34ee5682f30e12aa955949ed
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-11-11 18:50:59 +0530

    Time: 150 ms (45.37%), Space: 10.1 MB (81.43%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-    int beauty(const std::vector<int> &mapy) {
-        int mf = INT_MIN; // max frequency
-        int lf = INT_MAX; // min frequency (excluding zeros)
+    int beautySum(string s) {
+        int totalBeauty = 0;
         
-        for(int i = 0; i < 26; i++) {
-            if(mapy[i] > 0) {
-                if(mapy[i] > mf) {
-                    mf = mapy[i];
-                }
-                if(mapy[i] < lf) {
-                    lf = mapy[i];
-                }
-            }
-        }
-        
-        return (mf - lf);
-    }
+        for (int i = 0; i < s.length(); i++) {
+            vector<int> freq(26, 0);  // Frequency array for characters 'a' to 'z'
+            
+            for (int j = i; j < s.length(); j++) {
+                freq[s[j] - 'a']++;  // Update frequency for current character
 
-    int beautySum(const std::string &s) {
-        int res = 0;
-        
-        for(int i = 0; i < s.length(); i++) {
-            std::vector<int> mapy(26, 0);
-            for(int j = i; j < s.length(); j++) {
-                mapy[s[j] - 'a']++;
-                res += beauty(mapy);
+                // Calculate the beauty of the substring from i to j
+                int maxFreq = *max_element(freq.begin(), freq.end());
+                int minFreq = INT_MAX;
+                
+                // Find the minimum frequency of non-zero counts
+                for (int f : freq) {
+                    if (f > 0) minFreq = min(minFreq, f);
+                }
+                
+                totalBeauty += (maxFreq - minFreq);
             }
         }
         
-        return res;
+        return totalBeauty;
     }
 };
