9d9743ef46011b1527f04485fea060e547006230
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-11-15 20:34:34 +0530

    Time: 0 ms (100%), Space: 69.7 MB (25.81%) - LeetHub

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -1,42 +1,44 @@
 class Solution {
 public:
-    int findLengthOfShortestSubarray(vector<int>& arr) {
-        int n = arr.size();
+    int findLengthOfShortestSubarray(vector<int>& arr) 
+    {
+        int n=arr.size();
+        int left=0;
+        int right=n-1;
         
-        if (n == 1) {
+        for(int i=1; i<n; i++){
+            if(arr[i-1]<=arr[i]){
+                left=i;
+            }
+            else{
+                break;
+            }
+        }
+
+        for(int i=n-2; i>=0; i--){
+            if(arr[i]<=arr[i+1]){
+                right=i;
+            }
+            else{
+                break;
+            }
+        }
+
+        if(right==0){
             return 0;
         }
         
-        // Find the longest non-decreasing prefix
-        int left = 0;
-        while (left + 1 < n && arr[left] <= arr[left + 1]) {
-            left++;
-        }
-        
-        if (left == n - 1) {  // Array is already non-decreasing
-            return 0;
-        }
-        
-        // Find the longest non-decreasing suffix
-        int right = n - 1;
-        while (right > 0 && arr[right - 1] <= arr[right]) {
-            right--;
-        }
-        
-        // Initial result: remove everything between left and right
-        int result = min(n - left - 1, right);
-        
-        // Try to merge prefix and suffix
-        int i = 0, j = right;
-        while (i <= left && j < n && left<right) {
-            if (arr[i] <= arr[j]) {
-                result = min(result, j - i - 1);
+        int result=min(n-left-1,right);
+        int i=0,j=right;
+        while(i<=left and j<n){
+            if(arr[i]<=arr[j]){
+                result=min(result,j-i-1);
                 i++;
-            } else {
+            }
+            else{
                 j++;
             }
         }
-        
         return result;
     }
 };
