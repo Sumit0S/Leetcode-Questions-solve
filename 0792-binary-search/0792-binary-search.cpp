3c2c62be81fcf0690b5afff9d9a8696f23e7d350
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2024-11-19 18:36:14 +0530

    Time: 0 ms (100%), Space: 30.1 MB (89.04%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        
+    int search(vector<int>& nums, int target) 
+    {
         int low=0;
         int high=nums.size()-1;
-        int mid=low+((high-low)/2);
-
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]==target){
-                return i;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target){
+                return mid;
             }
-            else if(nums[i]<target){
-                high=mid-1;
-            }
-            else if(nums[i]>target){
+            if(nums[mid]<target){
                 low=mid+1;
             }
+            else{
+                high=mid-1;
+            }
+
         }
         return -1;
     }
