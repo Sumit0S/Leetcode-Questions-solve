bbba7f653c796f989cf44ff7974adf686cd06b78
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2025-01-14 23:23:53 +0530

    Time: 2 ms (51.61%), Space: 9.2 MB (76.56%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    bool isIsomorphic(string s, string t) {
-        map<char, char> map1;
-        map<char, char> map2;
+    bool isIsomorphic(string s, string t)
+    {
+       map<char,char> mp;
+       map<char,char> mp2;
 
-        for(int i=0; i<s.size(); i++){
-        char c1 = s[i];
-        char c2 = t[i];
+       for(int i=0; i<min(s.length(),t.length()); i++)
+       {
 
-        // Check if there is already a mapping for c1
-        if (map1.find(c1) != map1.end()) {
-            // If the current mapping does not match the previous mapping, return false
-            if (map1[c1] != c2) {
-                return false;
+            if(mp.find(s[i])!=mp.end() or mp2.find(t[i])!=mp2.end()){
+                if(mp.find(s[i])!=mp.end()){
+                    if(mp[s[i]]!=t[i]){
+                        return false;
+                    }
+                }
+                else{
+                    if(mp2[t[i]]!=s[i]){
+                        return false;
+                    }
+                }
             }
-        } else {
-            // Otherwise, create a new mapping from c1 to c2
-            map1[c1] = c2;
-        }
-        if (map2.find(c2) != map2.end()) {
-            // If the current mapping does not match the previous mapping, return false
-            if (map2[c2] != c1) {
-                return false;
+            else
+            {
+                mp[s[i]]=t[i];
+                mp2[t[i]]=s[i];
             }
-        } else {
-            // Otherwise, create a new mapping from c2 to c1
-            map2[c2] = c1;
-        }
-
-        }
-        return true;
+       }
+       
+       return true;
     }
 };
