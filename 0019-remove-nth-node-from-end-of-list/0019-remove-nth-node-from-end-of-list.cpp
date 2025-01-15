6ffb03a7ecd16133c6f629c42992d2972beef35d
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2025-01-16 01:31:28 +0530

    Time: 0 ms (100%), Space: 15.1 MB (21.11%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -10,29 +10,27 @@
  */
 class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* &head, int n) {
-    ListNode* fastp = head;
-    ListNode* slowp = head;
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int cnt=0;
 
-    // Move the fastp pointer N nodes ahead
-    for (int i = 0; i < n; i++)
-        fastp = fastp->next;
-
-    // If fastp becomes NULL,
-    // the Nth node from the end is the head
-    if (fastp == NULL)
-        return head->next;
-
-    // Move both pointers until fastp reaches the end
-    while (fastp->next != NULL) {
-        fastp = fastp->next;
-        slowp = slowp->next;
-    }
-
-    // Delete the Nth node from the end
-    ListNode* delNode = slowp->next;
-    slowp->next = slowp->next->next;
-    delete delNode;
-    return head;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            cnt+=1;
+            temp=temp->next;
+        }
+        ListNode* node=head;
+        ListNode* prev=head;
+        if(cnt==n){
+            return node->next;
+        }
+        while(cnt!=n)
+        {
+            prev=node;
+            node=node->next;
+            cnt--;
+        }
+        prev->next=node->next;
+        
+        return head;
     }
 };
