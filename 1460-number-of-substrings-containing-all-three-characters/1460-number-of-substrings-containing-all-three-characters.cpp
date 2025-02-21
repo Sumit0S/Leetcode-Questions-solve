42987923da57b4a48d05a5e9486681a7aa13a647
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2025-02-22 03:11:13 +0530

    Time: 48 ms (6.17%), Space: 16.8 MB (9.54%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    int numberOfSubstrings(string s)
-    {
-        int lastseen[3]={-1,-1,-1};
-        int cnt=0;
-        for(int r=0; r<s.size(); r++)
-        {
-            lastseen[s[r]-'a']=r;
-            if(lastseen[0]!=-1 and lastseen[1]!=-1 and lastseen[2]!=-1){
-                cnt+=1+min(lastseen[0],min(lastseen[1],lastseen[2]));
-            }
 
-        }
-        return cnt;
+    int atMost(string &nums, int k) {
+        if (k < 0) return 0;
+        cout<<" --"<<k<<"-- ";
+        unordered_map<char,int> mp;
+        int l = 0, sum = 0, count = 0;
+        for (int r = 0; r < nums.size(); ++r) {
+            mp[nums[r]]++;
+            while (mp.size() > k) {
+                mp[nums[l]]--;
+             if (mp[nums[l]] == 0) {
+                    mp.erase(nums[l]); // Remove character if count becomes zero
+            }
+                l++;
+            }
+            count += (r - l + 1);
+    }
+        return count;
+    }
+    int numberOfSubstrings(string s) {
+        return atMost(s,3)-atMost(s,2);
     }
 };
