76cdf4addf29c0a953200f226b149c72e29cc760
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2025-02-22 23:40:33 +0530

    Time: 125 ms (5.03%), Space: 360.9 MB (5.03%) - LeetHub

--- 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.cpp
+++ 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.cpp
@@ -9,38 +9,40 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
 class Solution {
 public:
-    TreeNode* recoverFromPreorder(string traversal) {
-        stack<TreeNode*> st;
-        int i = 0, n = traversal.size();
 
-        while (i < n) {
-            int depth = 0;
-            while (i < n && traversal[i] == '-') { 
-                depth++; 
-                i++;
-            }
-            
-            int num = 0;
-            while (i < n && isdigit(traversal[i])) { 
-                num = num * 10 + (traversal[i] - '0'); 
-                i++;
-            }
-
-            TreeNode* node = new TreeNode(num);
-
-            while (st.size() > depth) st.pop();
-
-            if (!st.empty()) {
-                if (!st.top()->left) st.top()->left = node;
-                else st.top()->right = node;
-            }
-
-            st.push(node);
+    TreeNode* solve(string traversal,int &i,int depth,int n){
+        if(i>=n){
+            return NULL;
         }
+        int j=i;
+        while(j<n and traversal[j]=='-'){
+            j++;
+        }
+        int dash=j-i;
+        if(depth!=dash){
+            return NULL;
+        }
+        i+=dash;
+        int num=0;
+        while(j<n and isdigit(traversal[i])){
+            num=(num*10) + traversal[i]-'0';
+            i++;
+        }
+        TreeNode* node=new TreeNode(num);
+        node->left=solve(traversal,i,depth+1,n);
+        node->right=solve(traversal,i,depth+1,n);
+        return node;
 
-        while (st.size() > 1) st.pop();
-        return st.top();
+    }
+
+    TreeNode* recoverFromPreorder(string traversal) {
+        int i=0;
+        int depth=0;
+        int n=traversal.size();
+        return solve(traversal,i,depth,n);
     }
 };
