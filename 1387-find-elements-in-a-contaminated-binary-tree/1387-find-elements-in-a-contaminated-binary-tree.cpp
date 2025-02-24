7e90f6726506998f4a4d736ba834911250a604b2
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2025-02-25 00:21:42 +0530

    Time: 13 ms (29.41%), Space: 34.8 MB (14.65%) - LeetHub

---
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class FindElements {
+public:
+
+    void dfs(TreeNode* root,int x){
+        if(root==NULL){
+            return;
+        }
+        root->val=x;
+        s.insert(x);
+        dfs(root->left,(2*x)+1);
+        dfs(root->right,(2*x)+2);
+        return;
+      }
+    unordered_set<int> s;
+    FindElements(TreeNode* root)
+    {
+        // s.clear();
+        dfs(root,0);
+    }
+    
+    bool find(int target) {
+        return s.count(target);
+    }
+};
