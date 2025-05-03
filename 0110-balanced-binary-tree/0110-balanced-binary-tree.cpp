13357bec54ae63c75540c290803f74dd7d926279
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2025-05-03 19:15:35 +0530

    Time: 0 ms (100%), Space: 22.9 MB (97.82%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -12,31 +12,24 @@
 class Solution {
 public:
 
-    pair<bool,int> balanced(TreeNode* root)
+
+
+    pair<bool,int> check(TreeNode* root)
     {
         if(root==NULL){
-            pair<bool,int> p = make_pair(true,0);
-            return p;
+            return {true,0};
         }
-        pair<bool,int> left=balanced(root->left);
-        pair<bool,int> right=balanced(root->right);
+        pair<bool,int> left=check(root->left); // t,0
+        pair<bool,int> right=check(root->right); // t,0
 
-        bool dif=abs(left.second-right.second)<=1;
+        bool isbalanced =left.first && right.first && abs(left.second - right.second) <= 1;
 
-        pair<bool,int> ans;
-        ans.second=max(left.second,right.second)+1;
-        if(left.first and right.first and dif)
-        {
-            ans.first=true;
-        }
-        else{
-            ans.first=false;
-        }
-        return ans;
+        int height=max(left.second,right.second)+1;
+        return {isbalanced,height};
     }
-
     bool isBalanced(TreeNode* root) 
     {
-        return balanced(root).first;
+        pair<bool,int> a=check(root);
+        return a.first;
     }
 };
