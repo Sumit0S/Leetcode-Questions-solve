26166fbe5a038a21ca783e2d7a5a237a4a033ddc
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2025-06-06 16:53:12 +0530

    Time: 0 ms (100%), Space: 18.4 MB (25.93%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color)
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
     {
         int n=image.size();
         int m=image[0].size();
+        int row[4]={0,-1,0,1};
+        int col[4]={1,0,-1,0};
 
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        int startColor = image[sr][sc];
+
+        vector<vector<bool>> visited(n,vector<bool>(m,false));
         queue<pair<int,int>> q;
+
+        int c1=image[sr][sc];
         q.push({sr,sc});
-
-        vis[sr][sc] = 1;
-        image[sr][sc] = color; 
-
-        int nrow[]={0, 0, 1, -1};
-        int ncol[]={1, -1, 0, 0};
 
         while(!q.empty()){
             int r=q.front().first;
             int c=q.front().second;
             q.pop();
-
             for(int i=0; i<4; i++){
-                int nr=r+nrow[i];
-                int nc=c+ncol[i];
-                if(nr>=0 and nr<n and nc>=0 and nc<m and !vis[nr][nc] and image[nr][nc]==startColor){
-                    q.push({nr,nc});
-                    image[nr][nc]=color;
-                    vis[nr][nc]=1;
+                int crow=r+row[i];
+                int ccol=c+col[i];
+                if(crow>=0 and crow<n and ccol>=0 and ccol<m and !visited[crow][ccol] and image[crow][ccol]==c1){
+                    q.push({crow,ccol});
+                    image[crow][ccol]=color;
+                    visited[crow][ccol]=true;
                 }
             }
         }
+        image[sr][sc]=color;
         return image;
     }
 };
