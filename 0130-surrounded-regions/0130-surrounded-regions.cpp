873938fa9b5153f980ba07151d58ce401b44d4f5
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2025-06-12 16:23:02 +0530

    Time: 5 ms (13.46%), Space: 14.6 MB (42.49%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,70 +1,54 @@
 class Solution {
 public:
 
-    void dfstraversal(int r,int c,vector<vector<char>>& ans,vector<vector<int>> &vis)
-    {
-        int n=ans.size();
-        int m = ans[0].size();
-        vis[r][c]=1;
-        int delRow[] = {-1, 0, +1, 0};
-        int delCol[] = {0, +1, 0, -1}; 
+    void dfs(int row,int col,vector<vector<bool>> &visited,vector<vector<char>>& board) {
+        int n=board.size();
+        int m=board[0].size();
+
+        visited[row][col]=true;
+        int drow[4]={0,-1,0,1};
+        int dcol[4]={1,0,-1,0};
+        
         for(int i=0; i<4; i++){
-            int rdir=r+delRow[i];
-            int cdir=c+delCol[i];
-            if(rdir>=0 and rdir<n and cdir>=0 and cdir<m and !vis[rdir][cdir] and ans[rdir][cdir]=='O'){
-                dfstraversal(rdir,cdir,ans,vis);
+            int r=row+drow[i];
+            int c=col+dcol[i];
+            if(r>=0 and r<n and c>=0 and c<m and !visited[r][c] and board[r][c]=='O'){
+                dfs(r,c,visited,board);
             }
         }
     }
-    void solve(vector<vector<char>>& ans) 
-    {
-        int n=ans.size();
-        int m = ans[0].size();
-
-        vector<vector<char>> board=ans;
-        vector<vector<int>> vis(n,vector<int>(m,0));
-
-        for(int i=0; i<m; i++){
-            int row1=board[0][i];
-            int row2=board[n-1][i];
-            if(row1=='O')
-            {
-                if(!vis[0][i]){
-                    dfstraversal(0,i,ans,vis);
-                }
-            }
-            if(row2=='O'){
-                if(!vis[n-1][i]){
-                    dfstraversal(n-1,i,ans,vis);
-                }
-            }
-        }    
-
+    void solve(vector<vector<char>>& board) {
+        int n=board.size();
+        int m=board[0].size();
+        queue<pair<int,int>> q;
         for(int i=0; i<n; i++){
-            int row1=board[i][0];
-            int row2=board[i][m-1];
-            if(row1=='O')
-            {
-                if(!vis[i][0]){
-                    dfstraversal(i,0,ans,vis);
+            for(int j=0; j<m; j++){
+                if(board[i][j]=='O' and (i==0 or j==0 or i==n-1 or j==m-1)){
+                    q.push({i,j});
+                    cout<<i<<" "<<j<<endl;
                 }
             }
-            if(row2=='O'){
-                if(!vis[i][m-1]){
-                    dfstraversal(i,m-1,ans,vis);
-                }
-            }
+        }
+
+        vector<vector<bool>> visited(n,vector<bool>(m,false));
+
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            dfs(row,col,visited,board);
         }
 
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(vis[i][j]==1){
-                    ans[i][j]='O';
+                if(visited[i][j]){
+                    board[i][j]='O';
                 }
                 else{
-                    ans[i][j]='X';
+                    board[i][j]='X';
                 }
             }
         }
+
     }
 };
