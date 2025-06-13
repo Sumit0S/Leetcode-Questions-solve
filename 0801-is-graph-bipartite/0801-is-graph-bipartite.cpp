1aa92d245858c53a623da18e4b67da3bdc23f9b4
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2025-06-13 18:31:16 +0530

    Time: 0 ms (100%), Space: 17.4 MB (46.21%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-    int V = graph.size();
-    vector<int> color(V, -1);
-
-    for (int i = 0; i < V; ++i) {
-        if (color[i] == -1) {
-            queue<int> q;
-            q.push(i);
-            color[i] = 0;
-
-            while (!q.empty()) {
-                int node = q.front();
-                q.pop();   
-
-
-                for (int neighbor : graph[node]) {
-                    if (color[neighbor] == -1) {
-                        color[neighbor] = 1 - color[node];
-                        q.push(neighbor);
-                    } else if (color[neighbor] == color[node]) {
-                        return false;   
- // Graph is not bipartite
-                    }
-                }
+    bool is_bi(vector<vector<int>> &adj, vector<int> &visited, vector<int> &color, int col, int node) {
+        visited[node] = true;
+        color[node] = col;
+        
+        for (auto it : adj[node]) {
+            if (!visited[it]) {
+                if (!is_bi(adj, visited, color, 1 - col, it))
+                    return false;
+            } else if (color[it] == col) {
+                return false;  // Same color on adjacent nodes
             }
         }
+        return true;
     }
 
-    return true; // Graph is bipartite
+    bool isBipartite(vector<vector<int>> &graph) {
+        int n = graph.size();
+        vector<int> visited(n, false);
+        vector<int> color(n, -1);
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                if (!is_bi(graph, visited, color, 0, i))
+                    return false;
+            }
+        }
+        return true;
     }
 };
