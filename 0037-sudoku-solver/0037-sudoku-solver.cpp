638ecc441d8aac0f0744cc8805d01b8d3690a2b3
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2025-08-31 23:09:43 +0530

    Time: 137 ms (91.33%), Space: 8.5 MB (99.14%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,44 +1,54 @@
 class Solution {
-public:
-    bool insert(vector<vector<char>>& board)
-    {
-        for(int i=0; i<board.size(); i++){
-            for(int j=0; j<board[0].size(); j++){
-                if(board[i][j]=='.'){
-                    for(char k='1'; k<='9'; k++){
-                        
-                        if(is_valid(board,i,j,k)){
-                            board[i][j]=k;
-                            if(insert(board)==true){
-                                return true;
-                            }
-                            else{
-                                board[i][j]='.';
-                            } 
-                        }
+    int n = 3, N = 9;
+    int rows[9][10] = {}, cols[9][10] = {}, boxes[9][10] = {};
+    vector<vector<char>>* boardPtr;
+    bool sudokuSolved = false;
 
-                    }
-                    return false;
+    bool couldPlace(int d, int row, int col) {
+        int idx = (row / n) * n + col / n;
+        return rows[row][d] + cols[col][d] + boxes[idx][d] == 0;
+    }
+
+    void placeNumber(int d, int row, int col) {
+        int idx = (row / n) * n + col / n;
+        rows[row][d]++;
+        cols[col][d]++;
+        boxes[idx][d]++;
+        (*boardPtr)[row][col] = d + '0';
+    }
+
+    void removeNumber(int d, int row, int col) {
+        int idx = (row / n) * n + col / n;
+        rows[row][d]--;
+        cols[col][d]--;
+        boxes[idx][d]--;
+        (*boardPtr)[row][col] = '.';
+    }
+
+    void placeNextNumbers(int row, int col) {
+        if (row == N - 1 && col == N - 1) sudokuSolved = true;
+        else if (col == N - 1) backtrack(row + 1, 0);
+        else backtrack(row, col + 1);
+    }
+
+    void backtrack(int row, int col) {
+        if ((*boardPtr)[row][col] == '.') {
+            for (int d = 1; d <= 9; d++) {
+                if (couldPlace(d, row, col)) {
+                    placeNumber(d, row, col);
+                    placeNextNumbers(row, col);
+                    if (!sudokuSolved) removeNumber(d, row, col);
                 }
             }
-        }
-        return true;
+        } else placeNextNumbers(row, col);
     }
 
-    bool is_valid(vector<vector<char>> board,int row,int col,char c){
-    for (int i = 0; i < 9; i++) {
-        if (board[i][col] == c)
-        return false;
-
-        if (board[row][i] == c)
-        return false;
-
-        if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
-        return false;
-    }
-    return true;
-    }
+public:
     void solveSudoku(vector<vector<char>>& board) {
-        insert(board);
+        boardPtr = &board;
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                if (board[i][j] != '.') placeNumber(board[i][j] - '0', i, j);
+        backtrack(0, 0);
     }
 };
