2da062651288213f9b2ba19415f74451f60a4261
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2025-09-05 15:03:30 +0530

    Time: 6 ms (47.63%), Space: 23.8 MB (57.08%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
 
-        sort(intervals.begin(), intervals.end()); 
+    static bool comp(vector<int> &a, vector<int> &b){
+        return a[0]<b[0];
+    }
+    vector<vector<int>> merge(vector<vector<int>>& intervals)
+    {
+        int n=intervals.size();
+        sort(intervals.begin(),intervals.end(), comp);
 
-        vector<vector<int>> merged; 
+        vector<vector<int>> ans;
+        int i=0;
+        while(i<n){
+            int first=intervals[i][0];
+            int second=intervals[i][1];
 
-        for (const auto& interval : intervals) {
-            if (merged.empty() || merged.back()[1] < interval[0]) {
-                // No overlap, add the interval to the result
-                merged.push_back(interval);
-            } else {
-                // Overlap, merge with the last interval in the result
-                merged.back()[1] = max(merged.back()[1], interval[1]);
+            int j=i+1;
+            while(j<n and intervals[j][0]<=second){
+                second=max(second,intervals[j][1]);
+                j++;
             }
+            i=j;
+            ans.push_back({first,second});
         }
-
-        return merged;  
+        return ans;
     }
 };
