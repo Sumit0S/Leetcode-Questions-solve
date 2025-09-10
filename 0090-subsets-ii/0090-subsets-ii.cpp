90540f378ff48a59abe84e3f4264175b32620663
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2025-09-10 11:14:50 +0530

    Time: 0 ms (100%), Space: 10.5 MB (66.51%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
-    void subsetsum(vector<int> nums, int n,int index,vector<int> output,set<vector<int>> &ans)
-    {
-        if(index>=n){
-            sort(output.begin(), output.end());
-            ans.insert(output);
-            return;
-        }
-        // for(int i=index; i<nums.size(); i++){
-        //     if(nums[i]==nums[i-1]){
-        //         continue;
-        //     }
-            int a=nums[index];
-            output.push_back(a);
-            subsetsum(nums,n,index+1,output,ans);
-            output.pop_back();
-        // }
 
-        subsetsum(nums,n,index+1,output,ans);
-        return;
-        
-    }
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<int> output;
-        set<vector<int>> ans;
-        vector<vector<int>> ans1;
-        subsetsum(nums,nums.size(),0,output,ans);
-        for (auto it = ans.begin(); it != ans.end(); it++) {
-            ans1.push_back( * it);
+    void findans(int ind,int n,vector<vector<int>> &ans,vector<int>& temp,vector<int>& nums){
+        ans.push_back(temp);
+        for(int i=ind; i<n; i++)
+        {
+            if(i!=ind && nums[i]==nums[i-1]) continue;
+            temp.push_back(nums[i]);          
+            findans(i + 1, n, ans, temp, nums); 
+            temp.pop_back();
         }
-        return ans1;
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) 
+    {
+        sort(nums.begin(),nums.end());
+            vector<vector<int>> ans;
+            vector<int> temp;
+            findans(0,nums.size(),ans,temp,nums);
+            return ans;
     }
 };
