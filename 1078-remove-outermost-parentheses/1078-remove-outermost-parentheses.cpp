466ae3dda7cf4ef481ac9bcedc86f95c939aa1ae
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2025-09-24 15:40:58 +0530

    Time: 0 ms (100%), Space: 8.9 MB (80.48%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -2,27 +2,22 @@ class Solution {
 public:
     string removeOuterParentheses(string s) 
     {
-        string temp="";
+        int cnt=0;
         string ans="";
         for(auto i:s){
             if(i=='('){
-                temp.push_back('(');
-                if(temp.size()>1){
+                cnt+=1;
+                if(cnt>1){
                     ans.push_back(i);
                 }
             }
             else{
-                if(temp.size()>1){
+                cnt-=1;
+                if(cnt>=1){
                     ans.push_back(i);
-                    temp.pop_back();
-                }
-                else{
-                    if(!temp.empty()){
-                        temp.pop_back();
-                    }
                 }
             }
-        }  
-        return ans;  
+        }    
+        return ans;
     }
 };
