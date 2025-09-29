7f880b043f654de1d9f5a32ecc3c0f96e59689db
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2025-09-29 10:54:17 +0530

    Time: 0 ms (100%), Space: 9.3 MB (80.5%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    bool isIsomorphic(string s, string t) {
-        map<char,char> mp;
-        map<char,char> mp2;
-        int i=0;
-        int n=min(s.length(),t.length());
-        
-        while(i<n){
-            if((mp.find(s[i])!=mp.end()) || mp2.find(t[i])!=mp2.end())
-            {
-                // if(mp2[t[i]]!=t[i]){
-                //     return false;
-                // }
-                cout<<i<<" ";
-                if(mp[s[i]]!=t[i]){
+    bool isIsomorphic(string s, string t) 
+    {
+        set<char> visited;
+        unordered_map<char,char> mp;
+
+        for(int i=0; i<min(s.length(),t.length()); i++)
+        {
+            if(mp.find(s[i])!=mp.end()){
+                if(t[i]!=mp[s[i]]){
                     return false;
                 }
             }
-            
-            mp[s[i]]=t[i];
-            mp2[t[i]]=s[i];
-            i++;
+            else{
+                if(visited.find(t[i])!=visited.end()){
+                    return false;
+                }
+                visited.insert(t[i]);
+                mp[s[i]]=t[i];
+            } 
         }
         return true;
     }
