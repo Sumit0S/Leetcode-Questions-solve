4a9aad317e2d11f54e061f47adeda463d1009873
Sumit Sharma <167033891+Sumit0S@users.noreply.github.com>
2025-10-09 12:14:02 +0530

    Time: 0 ms (100%), Space: 15.3 MB (32.96%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -4,11 +4,13 @@ public:
     {
         int low=0;
         int high=arr.size()-1;
+
         while(low<=high){
-            int mid=low+(high-low)/2;
+            int mid=low + (high - low) / 2;
             if(arr[mid]==target){
                 return mid;
             }
+            // left half sorted
             if(arr[low]<=arr[mid]){
                 if(arr[low]<=target and target<=arr[mid]){
                     high=mid-1;
@@ -25,7 +27,7 @@ public:
                     high=mid-1;
                 }
             }
-        }  
+        }
         return -1;
     }
 };
